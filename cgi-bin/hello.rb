ac62e9022866fc11a33d8f4886fd240b35623314
John <johnbdonner@gmail.com>
2014-10-09 00:52:55 -0400

    trying to connect sqlite3... failing

--- cgi-bin/hello.rb
+++ cgi-bin/hello.rb
@@ -1,7 +1,36 @@
 #!/usr/bin/ruby
 
 require "cgi"
+require "sqlite3"
 
+=begin
 cgi = CGI.new
 
 form = cgi.params
+=end
+
+begin
+
+	db = SQLite3::Database.new ":memory:"
+	puts db.get_first_value 'SELECT SQLITE_VERSION()'
+
+rescue SQLite3::Exception => e
+
+	puts "Exception occured"
+	puts e
+
+ensure
+	db.close if db
+end
+
+=begin
+
+print cgi.header()
+
+puts "<html>"
+puts "<body>"
+puts "hello"
+puts "</body>"
+puts "</html>"
+
+=end
