2f7b4f76bc495c85f9586e9e08a1f906a6730cfd
John <johnbdonner@gmail.com>
2014-10-09 23:38:44 -0400

    working cookies and signup

--- cgi-bin/hello.rb
+++ cgi-bin/hello.rb
@@ -2,20 +2,68 @@
 
 require "cgi"
 require "sqlite3"
+require "securerandom"
 
 cgi = CGI.new
 
+user_sessionID = SecureRandom.uuid
 user_name = cgi['name']
 user_email = cgi['email']
 user_password = cgi['password']
 
-begin
-	db = SQLite3::Database.new ":memory:" # Change ':memory:' to 'users.db' to write to the database
-	db.execute "CREATE TABLE IF NOT EXISTS User(name varchar(100),
-				email varchar(100) PRIMARY KEY, password varchar(100))"
-	db.execute "INSERT INTO User VALUES('"+user_name+"',
-	 '"+user_email+"', '"+user_password+"')"
+cookie = cgi.cookies['user_id']
+if cookie.to_s() != '[]'
+	# extract sessionID from cookie to display
+	user_sessionID = cookie.value[0]
 
+	puts cgi.header()
+
+	puts "<html>"
+	puts "<body>"
+	puts "<p>Hello, " + user_name + ". You are already logged in.</p>"
+	puts "<p>SessionID: " + user_sessionID + "</p>"
+	puts "<p>There is a cookie!</p>"
+	puts "<p>cookie: '" + cookie.to_s() + "'</p>"
+	puts "<a href='/index.html'><button>Home</button></a>"
+	puts "</body>"
+	puts "</html>"
+else
+	# # # # # # # # # # # # # # # # #
+	# insert new user into database
+	# # # # # # # # # # # # # # # # #
+=begin
+	stm = db.prepare "SELECT * FROM User with name '"+user_name+"'" 
+    rs = stm.execute
+    if rs == user_name
+		db = SQLite3::Database.new ":memory:" # Change ':memory:' to 'users.db' to write to the database
+		db.execute "CREATE TABLE IF NOT EXISTS User(sessionID varchar(100), name varchar(100),
+					email varchar(100) PRIMARY KEY, password varchar(100))"
+		db.execute "INSERT INTO User VALUES('"+user_sessionID+"', '"+user_name+"',
+		 '"+user_email+"', '"+user_password+"')"
+=end
+		# create cookie
+		cookie = CGI::Cookie.new('name' => 'user_id',
+	                         'value' => user_sessionID,
+	                         'expires' => Time.now + 3600)
+		
+		puts cgi.header("cookie" => cookie)
+		# puts "cookie: " + cookie.to_s()
+		# cgi.out('cookie' => cookie)
+
+		puts "<html>"
+		puts "<body>"
+		puts "<p>Hello, " + user_name + ". Your account has been created.</p>"
+		puts "<p>Your associated email to this account is " + user_email + ".</p>"
+		puts "<p>SessionID: " + user_sessionID + "</p>"
+		puts "<p>You now have a NEW cookie! " + cookie.to_s() +"</p>"
+		puts "<a href='/index.html'><button>Home</button></a>"
+		puts "</body>"
+		puts "</html>"
+		
+	
+end
+
+=begin
 rescue SQLite3::Exception => e
 	puts "Exception occured"
 	puts e
@@ -23,6 +71,8 @@ rescue SQLite3::Exception => e
 ensure
 	db.close if db
 end
+
+=end
 
 =begin cookie code
 
@@ -54,13 +104,16 @@ cgi.out('cookie' => cookie) do
    cgi.head + cgi.body { cookie[0] }
 end
 
-=end
+=begin
 
-print cgi.header()
+puts cgi.header()
 
 puts "<html>"
 puts "<body>"
 puts "<p>Hello, " + user_name + ". Your account has been created.</p>"
 puts "<p>Your associated email to this account is " + user_email + ".</p>"
+puts "<p>SessionID: " + user_sessionID + "</p>"
 puts "</body>"
 puts "</html>"
+
+=end
