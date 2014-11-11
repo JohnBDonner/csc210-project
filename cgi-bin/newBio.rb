6abd6225b848cb47a439ec8ce565265fbbf191ce
John <johnbdonner@gmail.com>
2014-11-11 14:28:29 -0500

    updated JSON strings

--- cgi-bin/newBio.rb
+++ cgi-bin/newBio.rb
@@ -3,14 +3,10 @@
 
 require 'cgi'
 require 'sqlite3'
+require 'json'
 
 cgi = CGI.new
 
-=begin
-puts cgi.header("type" => "application/json")
-puts '{"name": "Alice"}'
-
-=end
 cookie = cgi.cookies['user_id']
 newBio = cgi['bioText']
 if newBio.nil?
@@ -26,22 +22,15 @@ if cookie.to_s() != '[]'
 						email varchar(100) PRIMARY KEY, password varchar(100), sessionID varchar(100), bio text);"
 		db.execute "UPDATE users SET bio = ? WHERE sessionID = ?;", [newBio, user_sessionID]
 		stm = db.prepare "SELECT * FROM users WHERE sessionID='"+user_sessionID+"';"
+
+		#stm = db.prepare "SELECT * FROM users WHERE sessionID='"+"e6b88e4a-3c59-4c60-a094-24c53f1fa916"+"';"
 		rs = stm.execute
 		db_user = rs.next_hash
 
 		puts cgi.header("type" => "application/json")
 		displayBio = db_user['bio']
-		if db_user['bio'].include? "\""
-			if displayBio.include? "\\"
-				displayBio = displayBio.gsub!("\\", "\\\\\\\\")
-			end
-			displayBio = displayBio.gsub!("\"", "\\\"")
-		elsif displayBio.include? "\\"
-			displayBio = displayBio.gsub!("\\", "\\\\\\\\")
-		else
-			displayBio = db_user['bio']
-		end
-		puts '{"bio": "'+displayBio+'"}'
+		myHash = {:bio => displayBio}
+		puts myHash.to_json
 
 	rescue SQLite3::Exception => e
 		puts "Exception occured"
