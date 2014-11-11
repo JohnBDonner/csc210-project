cca270f5bfefdd72f83ac5f33c7fffcf423a44f5
John <johnbdonner@gmail.com>
2014-11-11 01:16:03 -0500

    showUsers.rb successfully prints list of users

--- cgi-bin/showUsers.rb
+++ cgi-bin/showUsers.rb
@@ -5,34 +5,29 @@ require 'sqlite3'
 
 cgi = CGI.new
 
-
-=begin
-	IMPORTANT: I commented out some database lines, and switched the if statement,
-	so that way I can test for errors in terminal, and not in the browser, because
-	the browser just gives a 500 error and doesn't say anything.
-=end
 cookie = cgi.cookies['user_id']
-if cookie.to_s() == '[]'
+if cookie.to_s() != '[]'
 	### Found cookie, user is signed in ###
 	puts cgi.header()
 
-	#user_sessionID = cookie.value[0]
+	user_sessionID = cookie.value[0]
 	begin
 		db = SQLite3::Database.new "users.db"
 		# go into database
 		db.execute "CREATE TABLE IF NOT EXISTS users(name varchar(100),
 						email varchar(100) PRIMARY KEY, password varchar(100), sessionID varchar(100), bio text);"
-		#stm = db.prepare "SELECT * FROM users WHERE sessionID='"+user_sessionID+"';"
-		#rs = stm.execute
-		#db_user = rs.next_hash
+		stm = db.prepare "SELECT * FROM users WHERE sessionID='"+user_sessionID+"';"
+		rs = stm.execute
+		db_user = rs.next_hash
 
 		stm = db.prepare "SELECT * FROM users;"
 		rs = stm.execute
 
 		# Need to check information against signed in user...
 		puts "<h3>Users</h3>"
-		rs.each do |row|
-			puts "<li>" + row['name'] + "</li>"
+
+		while !rs.nil?
+			puts "<li>" + rs.next_hash['name'] + "</li>"
 		end
 
 	rescue SQLite3::Exception => e
@@ -41,7 +36,6 @@ if cookie.to_s() == '[]'
 	ensure
 		db.close if db
 	end
-
 else
 	### Did NOT find cookie, no user signed in ###
 	# redirect
