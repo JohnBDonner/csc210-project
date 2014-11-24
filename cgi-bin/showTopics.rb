0643ba36f020d00829c9e221cfa64c297b6a30df
John <johnbdonner@gmail.com>
2014-11-24 01:45:04 -0500

    added delete topics functionality via AJAX

--- cgi-bin/showTopics.rb
+++ cgi-bin/showTopics.rb
@@ -44,53 +44,33 @@ if cookie.to_s() != '[]'
 			tempTopic = topic_rs.next_hash
 			if !tempTopic.nil?
 				id = tempTopic['topic_id'].to_s
+				topic_user = tempTopic['user_id']
 				title = tempTopic['title']
 				desc = tempTopic['desc']
 				created_at = tempTopic['created_at']
 				updated_at = tempTopic['updated_at']
 
+				# add function to humanize the timestamp
 				currentTime = Time.now
 				updatedTime = Time.parse(updated_at)
 				values = updatedTime.to_a
 				updated_at = Time.local(*values).to_s
 
-				# puts "<a href='topic.rb?topic_id="+id+"'>"
 				puts "<div class='topicItem' id='topic_id_"+id+"'>"
 				puts "<a class='topicItem-title' href='topic.rb?topic_id="+id+"'>"+title+"</a>"
 				puts "<div class='topicItem-desc'>"+desc+"</div>"
 				puts "<div class='topicItem-updated'>Last updated "+updated_at+"</div>"
+				if db_user['user_id'] == topic_user
+					puts "<a class='topicItem-edit' id='topic_id_"+id+"' href='#'>Edit</a>"
+					# if topic has no contributions or followers
+					puts "<a class='topicItem-delete' id='topic_id_"+id+"' href='#'>Delete</a>"
+				end
 				puts "</div>"
 				# puts "</a>"
 			else
 				notNil = false
 			end
 		end
-
-=begin
-		stm = db.prepare "SELECT rowid, * FROM users;"
-		rs = stm.execute
-
-		# Need to check information against signed in user...
-		puts "<h3>Topics</h3>"
-
-		notNil = true
-		while notNil
-			tempUser = rs.next_hash
-			if !tempUser.nil?
-				email = tempUser['email']
-				name = tempUser['name']
-				id = tempUser['user_id'].to_s
-				if email != db_user['email']
-					puts "<li class='userItem' id='userid"+id+"'><a href='user.rb?id="+id+"'>"+name+"</a></li>"
-				else
-					puts "<li class='userItem' id='userid"+id+"'>"+name+"</li>"
-				end
-			else
-				notNil = false
-			end
-		end
-=end
-
 		puts '</div>'
 		puts '</div>'
 		puts '</body>'
